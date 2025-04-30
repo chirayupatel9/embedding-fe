97dddd5963142077f4e138c503574da3fcfc71ec
chirayupatel9 <chirayupatel911@gmail.com>
2025-04-30 14:21:18 -0400

    revert vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',     // bind to all network interfaces
-    port: 5173           // optional: you already exposed it in Docker
-  }
 });
