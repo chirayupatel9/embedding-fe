a758009f9cf8319a6e85253d14825427e70aba71
chirayupatel9 <chirayupatel911@gmail.com>
2025-04-30 18:59:00 -0400

    updated ep

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -3,7 +3,7 @@ const apiUrl = import.meta.env.VITE_API_URL;
 export const API_CONFIG = {
   BASE_URL: `${apiUrl}/api`,
   ENDPOINTS: {
-    EMBEDDINGS: '/embeddings',
+    EMBEDDINGS: '/dimensionality-reduction/tsne',
     GET_IMAGE: '/image-with-metadata',
   },
   TIMEOUT: 10000, // 30 seconds
